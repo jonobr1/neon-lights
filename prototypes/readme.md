e4b2a00fee1b35239cb66730d86e5f93d9a82b2a
Jono Brandel <jono@vrse.com>
2017-02-23 15:22:44 -0800

    added list view for prototypes

---
+++ prototypes/readme.md
@@ -0,0 +1,7 @@
+# Prototypes
+
++ [Controller Track](./controller-track.html)
++ [Diorama](./diorama.html)
++ [Forest](./forest-assets.html)
++ [Head Track]('./head-track.html')
++ [Blink Transition]('./modal-blink.html')
--- utils/markdown.js
+++ utils/markdown.js
@@ -2,27 +2,34 @@ var marked = require('marked');
 var path = require('path');
 var fs = require('fs');
 
-fs.readFile(path.resolve(__dirname, '../readme.md'), { encoding: 'utf8'}, function(err, contents) {
+[
+  '../readme.md',
+  '../prototypes/readme.md'
+].forEach(function(filepath, i) {
 
-  if (err) {
-    throw err;
-  }
-
-  var markdown = [
-    '<!doctype html><head><title>Neon Lights: Instructions</title>',
-    '<link type="text/css" rel="stylesheet" href="./release/styles/github-markdown.css">',
-    '</head><body class="markdown-body">',
-    marked(contents),
-    '</body></html>'
-  ].join('');
-
-  fs.writeFile(path.resolve(__dirname, '../index.html'), markdown, function(err) {
+  fs.readFile(path.resolve(__dirname, filepath), { encoding: 'utf8'}, function(err, contents) {
 
     if (err) {
       throw err;
     }
 
-    console.log('readme.md compiled with markdown.');
+    var markdown = [
+      '<!doctype html><head><title>Neon Lights: Instructions</title>',
+      '<link type="text/css" rel="stylesheet" href="./release/styles/github-markdown.css">',
+      '</head><body class="markdown-body">',
+      marked(contents),
+      '</body></html>'
+    ].join('');
+
+    fs.writeFile(path.resolve(__dirname, filepath.replace('readme.md', 'index.html')), markdown, function(err) {
+
+      if (err) {
+        throw err;
+      }
+
+      console.log(filepath, 'compiled with markdown.');
+
+    });
 
   });
 
