63b856a58b0d9ce2e512acfff7fa2af4f115e8b0
Jono Brandel <jono@vrse.com>
2017-02-23 15:24:26 -0800

    updated styles

--- utils/markdown.js
+++ utils/markdown.js
@@ -15,7 +15,9 @@ var fs = require('fs');
 
     var markdown = [
       '<!doctype html><head><title>Neon Lights: Instructions</title>',
-      '<link type="text/css" rel="stylesheet" href="./release/styles/github-markdown.css">',
+      '<link type="text/css" rel="stylesheet" href="',
+      (i <= 0) ? '.' : '..',
+      '/release/styles/github-markdown.css">',
       '</head><body class="markdown-body">',
       marked(contents),
       '</body></html>'
